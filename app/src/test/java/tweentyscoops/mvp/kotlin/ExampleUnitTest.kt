87e2089523ab339fbc87d38c6f0e4e35960af366
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-07-11 15:01:10 +0700

    improvment dependencies.

---
+++ app/src/test/java/tweentyscoops/mvp/kotlin/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package tweentyscoops.mvp.kotlin
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        Assert.assertEquals(1, 1)
+    }
+}
